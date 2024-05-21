28cb36b11935cc8db78aabd4e37a8162ed3ebc77
Ben_Lipsky <Benlipsky@icloud.com>
2024-05-21 13:48:52 -0400

    rg

---
+++ script.js
@@ -0,0 +1,9 @@
+function switchOne(){
+    document.getElementById('mainimg').src = 'Little\ Bird\ Toy\ Co\ Site\ Photos/block2.jpg'
+}
+function switchTwo(){
+    document.getElementById('mainimg').src = 'Little\ Bird\ Toy\ Co\ Site\ Photos/block1.jpg'
+}
+function switchThree(){
+    document.getElementById('mainimg').src = 'Little\ Bird\ Toy\ Co\ Site\ Photos/block3.jpg'
+}
